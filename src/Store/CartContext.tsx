61f2a4dbc4a771b714b5b73a4a6e37548c077777
Hazem Kawas <hazem.k.4488@gmail.com>
2022-03-15 15:40:49 +0100

    create card contexet and card provider

---
+++ src/Store/CartContext.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Product } from "../data";
+
+export interface CartContextState {
+  items: Product[];
+  totalAmount: 0;
+}
+
+const CartContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item: Product) => { },
+  removeItem: (id: string) => { },
+});
+
+export default CartContext;
