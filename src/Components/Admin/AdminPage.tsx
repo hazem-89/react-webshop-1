ccb5a737926305d2045a9c7ea141f5bd06a3743f
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2022-03-18 12:32:06 +0100

    styling admin form

--- src/Components/Admin/AdminPage.tsx
+++ src/Components/Admin/AdminPage.tsx
@@ -6,8 +6,11 @@ import { useFormik } from "formik";
 const AdminPage = () => {
     const formik = useFormik({
         initialValues: {
-            email: '',
-            password: '',
+            title: '',
+            brand: '',
+            price: '',
+            size: '',
+            description: '',
         },
         onSubmit: values => {
 
@@ -17,65 +20,79 @@ const AdminPage = () => {
     return(
         <Fragment>
             <form className={classes['form']}>
+                <h2 className={classes['admin-title']}>Add new product</h2>
                 <div className={classes['form-container']}>
                     <div className={classes['image-container']}>
                         <img src={image} alt="" />
                     </div>
                     <div className={classes['input-container']}>
-                        <div className={classes['email-input-container' && 'input-container']}>
-                            <label htmlFor="email">Email</label>
+                        <div className={classes['title-input-container' && 'input-item']}>
+                            <label htmlFor="title">Title</label>
                             <input
-                                id="email"
-                                name="email"
-                                type="email"
-                                placeholder="Freddie@example.com"
+                                id="title"
+                                name="title"
+                                type="title"
                                 onChange={formik.handleChange}
                                 onBlur={formik.handleBlur}
-                                value={formik.values.email}
+                                value={formik.values.title}
                             />
-                            {formik.touched.email && formik.errors.email ? <div className={classes.error}>{formik.errors.email}</div> : null}
+                            {formik.touched.title && formik.errors.title ? <div className={classes.error}>{formik.errors.title}</div> : null}
+                        </div>
 
-                            <div className={classes['email-input-container' && 'input-container']}>
-                                <label htmlFor="email">Email</label>
+                        <div className={classes['brand-input-container' && 'input-item']}>
+                            <label htmlFor="brand">Brand</label>
+                            <input
+                                id="brand"
+                                name="brand"
+                                type="brand"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.brand}
+                            />
+                            {formik.touched.brand && formik.errors.brand ? <div className={classes.error}>{formik.errors.brand}</div> : null}
+                        </div>
+
+                        <div className={classes['price-size-container']}>
+                            <div className={classes['price-input-container']}>
+                                <label htmlFor="price">Price</label>
                                 <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    placeholder="Freddie@example.com"
+                                    id="price"
+                                    name="price"
+                                    type="price"
                                     onChange={formik.handleChange}
                                     onBlur={formik.handleBlur}
-                                    value={formik.values.email}
+                                    value={formik.values.price}
                                 />
-                                {formik.touched.email && formik.errors.email ? <div className={classes.error}>{formik.errors.email}</div> : null}
+                                {formik.touched.price && formik.errors.price ? <div className={classes.error}>{formik.errors.price}</div> : null}
                             </div>
-                            <div>
-                                <div className={classes['email-input-container' && 'input-container']}>
-                                    <label htmlFor="email">Email</label>
-                                    <input
-                                        id="email"
-                                        name="email"
-                                        type="email"
-                                        placeholder="Freddie@example.com"
-                                        onChange={formik.handleChange}
-                                        onBlur={formik.handleBlur}
-                                        value={formik.values.email}
-                                    />
-                                    {formik.touched.email && formik.errors.email ? <div className={classes.error}>{formik.errors.email}</div> : null}
-                                </div>
-                                <div className={classes['email-input-container' && 'input-container']}>
-                                    <label htmlFor="email">Email</label>
-                                    <input
-                                        id="email"
-                                        name="email"
-                                        type="email"
-                                        placeholder="Freddie@example.com"
-                                        onChange={formik.handleChange}
-                                        onBlur={formik.handleBlur}
-                                        value={formik.values.email}
-                                    />
-                                    {formik.touched.email && formik.errors.email ? <div className={classes.error}>{formik.errors.email}</div> : null}
-                                </div>
+                            <div className={classes['size-input-container']}>
+                                <label htmlFor="size">Size</label>
+                                <input
+                                    id="size"
+                                    name="size"
+                                    type="size"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.size}
+                                />
+                                {formik.touched.size && formik.errors.size ? <div className={classes.error}>{formik.errors.size}</div> : null}
                             </div>
+                        </div>
+
+                        <div className={classes['description-input-container' && 'input-item']}>
+                            <label htmlFor="description">Description</label>
+                            <textarea
+                                id="description"
+                                name="description"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.description}
+                            />
+                            {formik.touched.description && formik.errors.description ? <div className={classes.error}>{formik.errors.description}</div> : null}
+                        </div>
+
+                        <div className={classes['form-btn-container']}>
+                            <button className={classes['form-btn']} type="submit">ADD PRODUCT</button>
                         </div>
                     </div>
                 </div>
