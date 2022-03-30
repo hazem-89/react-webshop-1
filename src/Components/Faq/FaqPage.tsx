962d7af76bc6906df2415ed852f445293d9c8726
rchtect <90619276+rchtect@users.noreply.github.com>
2022-03-30 19:03:50 +0200

    fixed issue where the functionality would break
    fixed issue where the functionality would break if not reloaded once since the cdn could catch up with the new page load

--- src/Components/Faq/FaqPage.tsx
+++ src/Components/Faq/FaqPage.tsx
@@ -1,11 +1,24 @@
+
 import FaqItem from "./FaqDataHandler";
 import { FaqCardData } from '../../data';
 import FaqQuestion from './FaqQuestion'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
 interface Props {
   faqs: any,
 }
 
 const Faq = (props: Props) => {
+  const reloadCount = Number(sessionStorage.getItem('reloadCount')) || 0;
+
+  useEffect(() => {
+    if(reloadCount < 2) {
+      sessionStorage.setItem('reloadCount', String(reloadCount + 1));
+      window.location.reload();
+    } else {
+      sessionStorage.removeItem('reloadCount');
+    }
+  }, []);
   return (
     <div className="flex flex-col items-center mt-[7rem]">
             <h2 className="text-themeColor font-bold text-4xl sm:text-6xl mb-2 text-left sm:mb-10">
